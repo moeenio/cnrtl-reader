d8607805f1837cd9ef50e289b57bb5fdd31a51a2
locness3 <locness3@orange.fr>
2020-08-08 08:57:53 +0200

    Code + search improvements - Use const instead of var - Use function declarations instead of expressions - Remove useless .thens in getArticle (renamed loadArticle) - Expand search field when typing - Trim useless whitespace from search - When loading an article, set the search field's value to the article - Use enter keyup instead of change to validate a search

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-var domParser = new DOMParser();
+const domParser = new DOMParser();
 
-var appProgressBar = document.querySelector(".app__progress-bar");
-var appProgressBarLoaded = document.querySelector(".app__progress-bar__loaded-fraction");
-var appBackButton = document.querySelector(".js-back-button");
-var appForwardButton = document.querySelector(".js-forward-button");
-var appMainSearch = document.querySelector(".js-app-main-search");
-var dictContentHolder = document.querySelector(".js-dict-content");
-var dictError = document.querySelector(".js-dict-error")
+const app = document.querySelector(".app");
+const appProgressBar = document.querySelector(".app__progress-bar");
+const appProgressBarLoaded = document.querySelector(".app__progress-bar__loaded-fraction");
+const appBackButton = document.querySelector(".js-back-button");
+const appForwardButton = document.querySelector(".js-forward-button");
+const appMainSearch = document.querySelector(".js-app-main-search");
+const dictContentHolder = document.querySelector(".js-dict-content");
+const dictError = document.querySelector(".js-dict-error")
 
-var setLoadProgress = percent => {
+function setLoadProgress (percent) {
   percent === 100 ? appProgressBar.classList.remove("visible") : appProgressBar.classList.add("visible");
   appProgressBarLoaded.style.width = percent + "%";
 };
 
-var getArticle = article => {
+function loadArticle (article) {
+  appMainSearch.value = article;
   dictContentHolder.innerHTML = "";
   dictError.style.display = "none";
   setLoadProgress(50);
-  fetch(`https://cors-anywhere.glitch.me/www.cnrtl.fr/definition/${article}`)
+  fetch(`https://cors-anywhere.herokuapp.com/www.cnrtl.fr/definition/${article}`)
   .then(response => response.text())
-  .then(responseText => domParser.parseFromString(responseText, "text/html"))
-  .then(responseDOM => {
+  .then(responseText => {
+    const responseDOM = domParser.parseFromString(responseText, "text/html")
     setLoadProgress(90);
-    return responseDOM.getElementById("lexicontent").outerHTML;
-  })
-  .then(responseDictContent => {
+    const responseDictContent = responseDOM.getElementById("lexicontent").outerHTML;
     dictContentHolder.innerHTML = responseDictContent;
     setLoadProgress(100);
   })
@@ -34,17 +34,27 @@ var getArticle = article => {
   });
 };
 
-appMainSearch.addEventListener("change", () => {
-  getArticle(appMainSearch.value);
-  history.pushState({ article: appMainSearch.value }, appMainSearch.value);
+appMainSearch.addEventListener("keyup", (e) => {
+  app.classList.add("searching");
+  if (e.key === "Enter") {
+    const searchTerm = appMainSearch.value.trim();
+    loadArticle(searchTerm);
+    history.pushState({ article: appMainSearch.value }, appMainSearch.value);
+    appMainSearch.blur();
+  }
 });
+
+appMainSearch.addEventListener("blur", () => {
+  app.classList.remove("searching");
+})
 
 appBackButton.addEventListener("click", () => history.back());
 appForwardButton.addEventListener("click", () => history.forward());
 
 window.onpopstate = () => {
+  console.log("popstate");
   if (history.state) {
-    getArticle(history.state.article);
+    loadArticle(history.state.article);
   }
   else {
     dictContentHolder.innerHTML = "";
